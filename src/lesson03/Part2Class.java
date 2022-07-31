fa8d9177a7f2b7417731ff038460543efe2537e8
lisatabolina <elizatabolina@gmail.com>
2022-07-31 20:55:43 +0300

    Создала папки для первых двух занятий

--- src/lesson03/Part2Class.java
+++ src/lesson03/Part2Class.java
@@ -1,3 +1,5 @@
+package lesson03;
+
 import java.util.Scanner;
 
 public class Part2Class {
@@ -5,32 +7,32 @@ public class Part2Class {
 
         /* 1. При помощи цикла for вывести на экран нечетные числа от 1 до 99. (использовать инкремент) */
 
-        for (int i = 1; i < 100; i++) {
-
-            if (i % 2 != 0) {
-                System.out.println(i);
-            }
-        }
+//        for (int i = 1; i < 100; i++) {
+//
+//            if (i % 2 != 0) {
+//                System.out.println(i);
+//            }
+//        }
 
 
         /* 2. При помощи цикла for вывести на экран числа от 15 до 1. (использовать декремент) */
 
-        for (int i = 15; i > 0; i--) {
-            System.out.println(i);
-        }
+//        for (int i = 15; i > 0; i--) {
+//            System.out.println(i);
+//        }
 
 
        /* 3. Напишите программу, где пользователь вводит любое целое положительное число N. Программа
        суммирует все числа от 1 до N. Для ввода числа используйте класс Scanner. */
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter any positive number: ");
-        int N = sc.nextInt();
-        int sum = 0;
-        for (int i = 1; i <= N; i++) {
-            sum += i;
-        }
-        System.out.println(sum);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter any positive number: ");
+//        int N = sc.nextInt();
+//        int sum = 0;
+//        for (int i = 1; i <= N; i++) {
+//            sum += i;
+//        }
+//        System.out.println(sum);
 
 
 
@@ -39,22 +41,29 @@ public class Part2Class {
        /* 4. Напишите программу, которая будет выводить на экран такую последовательность: 7 14 21 28 35 42
         49 56 63 70 77 84 91 98. В решении использовать цикл while */
 
-        int b = 0;
+//        int b = 0;
+//
+//        while (b < 14) {
+//            b++;
+//            System.out.print(7 * b + " ");
+//        }
 
-        while (b < 14) {
-            b++;
-            System.out.print(7 * b + " ");
-        }
+//        int i = 7;
+//        int k = 1;
+//        while (i < 99) {
+//            System.out.println(i *k);
+//            k++;
+//        }
 
 
 
         /* 5. Вывести 10 первых чисел последовательности 0, -5, -10, -15… */
 
 
-        for (int i = 0, m = 0; m < 10; m++) {
-            System.out.println(i);
-            i -= 5;
-        }
+//        for (int i = 0, m = 0; m < 10; m++) {
+//            System.out.println(i);
+//            i -= 5;
+//        }
 
 
 
@@ -62,7 +71,7 @@ public class Part2Class {
 
 
         for (int i = 10; i < 21; i++) {
-            System.out.println(i * i);
+            System.out.print(i * i + " ");
         }
     }
 }
